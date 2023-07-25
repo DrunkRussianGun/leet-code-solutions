e46cc9acc8d0052d4b60c0ade56ea75f6e12b841
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-07-25 23:24:16 +0400

    Solved the problem

--- problem-1/Problem1/Solution.cs
+++ problem-1/Problem1/Solution.cs
@@ -1,7 +1,23 @@
-﻿namespace Problem1;
+﻿using System;
+using System.Collections.Generic;
 
-public class Solution {
-	public int[] TwoSum(int[] nums, int target) {
-		
+namespace Problem1;
+
+public class Solution
+{
+	public int[] TwoSum(int[] numbers, int target)
+	{
+		var possibleOperandIndexes = new Dictionary<int, int>();
+		for (var secondIndex = 0; secondIndex < numbers.Length; ++secondIndex)
+		{
+			var second = numbers[secondIndex];
+			var first = target - second;
+			if (possibleOperandIndexes.TryGetValue(first, out var firstIndex))
+				return new[] { firstIndex, secondIndex };
+
+			possibleOperandIndexes.TryAdd(second, secondIndex);
+		}
+
+		return Array.Empty<int>();
 	}
 }
