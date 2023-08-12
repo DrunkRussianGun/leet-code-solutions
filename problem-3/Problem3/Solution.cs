bff9c219e8b0797f7d6ced5f3524cebb07a07171
Drunk Russian Gun <drunkrussiangun@gmail.com>
2023-08-12 04:39:43 +0400

    Solved the problem

--- problem-3/Problem3/Solution.cs
+++ problem-3/Problem3/Solution.cs
@@ -1,7 +1,43 @@
-﻿namespace Problem3;
+﻿using System;
+using System.Collections.Generic;
 
-public class Solution {
-	public int LengthOfLongestSubstring(string s) {
-		
+namespace Problem3;
+
+public class Solution
+{
+	public int LengthOfLongestSubstring(string input)
+	{
+		var longestSubstringLength = 0;
+
+		var seenChars = new HashSet<char>();
+		var leftBound = 0;
+		var nextCharIndex = 0;
+		while (true)
+		{
+			char nextChar = default;
+			while (nextCharIndex < input.Length)
+			{
+				nextChar = input[nextCharIndex];
+				if (!seenChars.Add(nextChar))
+					break;
+
+				++nextCharIndex;
+			}
+
+			longestSubstringLength = Math.Max(longestSubstringLength, nextCharIndex - leftBound);
+			if (nextCharIndex >= input.Length)
+				break;
+
+			char leftBoundChar;
+			do
+			{
+				leftBoundChar = input[leftBound];
+				seenChars.Remove(leftBoundChar);
+				++leftBound;
+			}
+			while (leftBoundChar != nextChar); // nextChar must be initialized by this point
+		}
+
+		return longestSubstringLength;
 	}
 }
